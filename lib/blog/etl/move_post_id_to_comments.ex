66a7c1c1303b262b7a0e199aff0595d141fb6c80
R.J. Osborne <rj@scriptdrop.co>
2019-04-02 07:42:53 -0400

    Refactored

--- lib/blog/etl/move_post_id_to_comments.ex
+++ lib/blog/etl/move_post_id_to_comments.ex
@@ -29,27 +29,31 @@ defmodule Blog.ETL.MovePostIdToComments do
     end
   end
 
-  def run() do
-    Blog.Repo.transaction(fn() -> 
-        comment_and_post_ids_stream()
-        |> Stream.each(fn {comment_id, post_id} ->
-            post =
-              Blog.Repo.get!(Blog.ETL.MovePostIdToComments.Post, post_id)
+  alias Blog.ETL.MovePostIdToComments.{Post, Comment, PostComment}
+  alias Blog.Repo
 
-             Blog.Repo.get!(Blog.ETL.MovePostIdToComments.Comment, comment_id)
-             |> Blog.Repo.preload(:post)
-             |> Ecto.Changeset.change(post: post)
-             |> Blog.Repo.update()
-        end)
-        |> Enum.count()
+  def run() do
+    Repo.transaction(fn ->
+      comment_and_post_ids_stream()
+      |> Stream.each(&update_comment/1)
+      |> Enum.count()
     end)
+  end
+
+  defp update_comment({comment_id, post_id}) do
+    post = Repo.get!(Post, post_id)
+
+    Repo.get!(Comment, comment_id)
+    |> Repo.preload(:post)
+    |> Ecto.Changeset.change(post: post)
+    |> Repo.update!()
   end
 
   defp comment_and_post_ids_stream() do
     from(
-      pc in Blog.ETL.MovePostIdToComments.PostComment,
+      pc in PostComment,
       select: {pc.comment_id, pc.post_id}
     )
-    |> Blog.Repo.stream()      
+    |> Repo.stream()
   end
 end
--- test/blog/etl/move_post_id_to_comments_test.exs
+++ test/blog/etl/move_post_id_to_comments_test.exs
@@ -1,25 +1,25 @@
 defmodule Blog.ETL.MovePostIdToCommentsTest do
-    use Blog.DataCase
-    alias Blog.ETL.MovePostIdToComments.{Post, Comment, PostComment}
-    alias Blog.Repo
+  use Blog.DataCase
+  alias Blog.ETL.MovePostIdToComments.{Post, Comment, PostComment}
+  alias Blog.Repo
 
-    describe "updating existing data to relate a comment to a post" do
-        test "a comment belongs_to a post" do
-            post =
-             %Post{}
-             |> Repo.insert!()
+  describe "updating existing data to relate a comment to a post" do
+    test "a comment belongs_to a post" do
+      post =
+        %Post{}
+        |> Repo.insert!()
 
-            comment =
-             %Comment{}
-             |> Repo.insert!()
+      comment =
+        %Comment{}
+        |> Repo.insert!()
 
-             %PostComment{post_id: post.id, comment_id: comment.id}
-             |> Repo.insert!()
+      %PostComment{post_id: post.id, comment_id: comment.id}
+      |> Repo.insert!()
 
-            Blog.ETL.MovePostIdToComments.run()
+      Blog.ETL.MovePostIdToComments.run()
 
-            result = Repo.get!(Comment, comment.id)
-            assert result.post_id == post.id
-        end
+      result = Repo.get!(Comment, comment.id)
+      assert result.post_id == post.id
     end
+  end
 end
